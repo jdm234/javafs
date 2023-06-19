2225296108c5178e343c7289e810e1d2360527ae
jdm234 <jdm234@naver.com>
2023-06-19 17:00:08 +0900

    0619

---
+++ 70_Project/js/script.js
@@ -0,0 +1,37 @@
+$(function(){
+    /
+    $(window).scroll(function () {
+        if ($(this).scrollTop >= 150) {
+            if (state === 0) {
+                pgbar(95)
+                pgbar(85)
+                pgbar(75)
+                bar();
+                state = 1;
+            } else {
+                return;
+            }
+        } else {
+            state = 0;
+        }
+    });
+    
+    
+    
+    
+    function pgbar(idx, maxCnt) {
+        var elem = $(".progress");
+        var w = 0;
+        var timer = setInterval(bar, 30);
+    
+        function bar() {
+            if (w >= maxCnt) {
+                clearInterval(timer);
+            } else {
+                w++;
+                elem.eq(idx).find({ width: w + "%" });
+                elem.eq(idx).find(w + "%");
+            }
+        }
+    }
+});
