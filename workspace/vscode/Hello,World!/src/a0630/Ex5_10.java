add919c393360e70fae15c6a6e6ccbb37cb339a2
jdm234 <jdm234@naver.com>
2023-06-30 17:49:10 +0900

    0630

---
+++ workspace/vscode/Hello,World!/src/a0630/Ex5_10.java
@@ -0,0 +1,13 @@
+public class Ex5_10 {
+    public static void main(String[] args) {
+        int[] arr[] = {
+            { 5, 5, 5, 5, 5 },
+            { 10, 10, 10, 10, 10 },
+            { 20, 20, 20, 20, 20 },
+            { 30, 30, 30, 30, 30}
+        };
+
+        int total = 0;
+        float avg = 0;
+    }
+}
---
+++ workspace/vscode/Hello,World!/src/a0630/Ex5_9.java
@@ -0,0 +1,32 @@
+public class Ex5_9 {
+    public static void main(String[] args) {
+        int[][] score = {
+                    { 100, 200, 300}
+                   ,{ 20, 20, 20 }
+                   ,{ 30, 30, 30 }
+                   ,{ 40, 40, 40}
+        };
+        // 과목별 총점
+        int korTotal = 0, engTotal = 0, mathTotal = 0;
+        
+        System.out.println("번호  국어  수학  총점  평균 ");
+        System.out.println("=============================");
+        for(int i = 0; i < score.length; i++){
+            int sum = 0;   // 개인별 총점
+            float avg = 0.0f;  //개인별 평균
+
+            korTotal = korTotal + score[i][0];
+            engTotal = engTotal + score[i][0];
+            mathTotal += score[i][2]; 
+            System.out.printf("%3d", i+1);
+             for (int j = 0; j < score[i].length; j++){
+                sum = sum + score[i][j];
+                System.out.printf("%5d", score[i][j]); // 기본 점수
+             }
+             avg = sum / (float) score[i].length; // 평균 계산
+             System.out.printf("5%d %5.1f%n", sum, avg);
+        }
+        System.out.println("=============================");
+        System.out.printf("총점: %3d %4d %4dn", korTotal, engTotal, mathTotal);
+    }
+}
