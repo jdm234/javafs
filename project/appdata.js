97f32f17d71369de0eb66f694060e9b57e513c41
javsfs <counman4@naver.com>
2023-08-25 17:51:01 +0900

    0825

--- project/appdata.js
+++ project/appdata.js
@@ -1,53 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const RecordForm = ({numList, setNumList}) =>{
-  const [num, setNum] = useState(0);
-  return(
-    <div>
-      <div>현재 숫자 : {num}</div>
-      <button onClick={()=>{setNum(num+1)}}>숫자 증가</button>
-      <button onClick={()=>{setNum(num-1)}}>숫자 감소</button>
-      <button onClick={()=>{setNum(0)}}>숫자 초기화</button>
-      <hr/>
-      <button onClick={()=>setNumList([...numList, num])}>숫자 기록</button>
-      <button onClick={()=>setNumList([])}>기록초기화</button>
-    </div>
-  )
-}
-const RecordList = ({numList}) =>{
-  return (
-    <div>
-      <h2>기록된 숫자</h2>
-      {numList.length > 0 ?<div>기록있음</div> : <div>기록 없음</div>}
-    </div>
-  )
-}  
 const App = () => {
+  const [text, setText] = useState([111]);
+  const [edit, setEdit] = useState([]);
+  let content = <div>{text}
+  <button onClick={() => setEdit(true)}>수정</button>
 
-  const [numList, setNumList] = useState([]);
-  return(
-    <div style={{margin:"40px auto",width:"800px",textAlign :"center"}}>
-        <RecordForm numList = {numList} setNumList = {setNumList}/>
-        <RecordList numList = {numList}/>
+  if(edit){
+    content=<div>
+      <input type="text"
+      value={text}
+      onChange={(e) =>{
+        setText(e.target.value);
+      }}
+    />
+    <button onClick={() => setEdit(false)}>수정</button>
     </div>
+  }
 
-
+  return (
+  <>
+    {conent}
+  </>
   )
 
-}
 
-function App() {
-    /* const { num, setNum } = useState(0); /* 현재의 숫자를 타타냄 초기화 값 0버튼으로 증가감소 */
-    const [numList, setNumList] = useState([]); /* 기록된 숫자를 저장할 배열 처음에는 빈배열로 시작하며, '숫자기록버튼이 클릭될때 업데이트 된다.' */
+export default App;
+
+{/* function App() {
+     const { num, setNum } = useState(0);  현재의 숫자를 타타냄 초기화 값 0버튼으로 증가감소
+    const [numList, setNumList] = useState([]); /* 기록된 숫자를 저장할 배열 처음에는 빈배열로 시작하며, '숫자기록버튼이 클릭될때 업데이트 된다.'
     function numRecoding() {
         setNumList([...numList, num]);
         setNum(0);
     } 
-}  */
-return (
-    {/* <div className="App">
+}    */}
+ {/* return (
+    {<div className="App">
         <div>현재숫자 : {num}</div>
         <button onclick={() => { setNum(num + 1) }} 숫자 증가></button>
         <button onclick={() => { setNum(num - 1) }} 숫자 감소></button>
@@ -58,7 +48,6 @@ return (
                 <li>{num}</li>
             })}
         </ul>
-    </div> */}
-);
+    </div>}
+);  */}
 
-export default App;
