3aa2c3cb9becf290e2a1161a283146dccf952573
javsfs <counman4@naver.com>
2023-09-07 17:49:57 +0900

    0907

--- project/racking.js
+++ project/racking.js
@@ -1,19 +1,37 @@
-const newsTicker = (timer) => {
-    const $ul = document.querySelector('ul');
+$(function() {
+  function animateRankList() {
+  var count = $('#rank-list li').length;
+  var height = $('#rank-list li').height();
   
-    window.setInterval(() => {
-      $ul.style.transitionDuration = "400ms";
-      $ul.style.marginTop = "-34px";
-  
-      window.setTimeout(() => {
-        $ul.style.transitionDuration = "";
-        $ul.style.marginTop = "";
-        // send the first element to the back 400ms later.
-        $ul.appendChild($ul.querySelector("li:first-child"));
-      }, 400)
-  
-    }, timer)
+  function step(index) {
+  $('#rank-list ol').delay(2000).animate({
+  top: -height * index,
+  }, 500, function() {
+  step((index + 1) % count);
+  });
   }
   
-  newsTicker(1500)
+  step(1);
+  }
   
+  window.yqlCallback = function(data) {
+  $(data.query.results.channel.data.naver.item).each(function(index, item) {
+  var text = item.rank + ' - ' + item.keyword + ' [' + item.upcount + ']';
+  $('#rank-list li a').eq(index).text(text);
+  });
+  
+  animateRankList();
+  }
+  
+  $('#rank-list li a').text('');
+  
+  var url = 'http://bangabmoa.com/api/hitkeyword.php?type=xml';
+  var yql = 'SELECT * FROM xml WHERE url="' + url + '"';
+  
+  $.ajax({
+  url: 'http://query.yahooapis.com/v1/public/yql?q=' + yql +
+  '&format=json&callback=yqlCallback',
+  dataType: 'script'
+  });
+  });
+    
