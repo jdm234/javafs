aa9a0ff5fd74a6123745d87fa4b9e42ff92579c0
javsfs <counman4@naver.com>
2023-09-11 17:58:40 +0900

    0911

--- project/racking.js
+++ project/racking.js
@@ -1,37 +1,14 @@
-$(function() {
-  function animateRankList() {
-  var count = $('#rank-list li').length;
-  var height = $('#rank-list li').height();
-  
-  function step(index) {
-  $('#rank-list ol').delay(2000).animate({
-  top: -height * index,
-  }, 500, function() {
-  step((index + 1) % count);
-  });
-  }
-  
-  step(1);
-  }
-  
-  window.yqlCallback = function(data) {
-  $(data.query.results.channel.data.naver.item).each(function(index, item) {
-  var text = item.rank + ' - ' + item.keyword + ' [' + item.upcount + ']';
-  $('#rank-list li a').eq(index).text(text);
-  });
-  
-  animateRankList();
-  }
-  
-  $('#rank-list li a').text('');
-  
-  var url = 'http://bangabmoa.com/api/hitkeyword.php?type=xml';
-  var yql = 'SELECT * FROM xml WHERE url="' + url + '"';
-  
-  $.ajax({
-  url: 'http://query.yahooapis.com/v1/public/yql?q=' + yql +
-  '&format=json&callback=yqlCallback',
-  dataType: 'script'
-  });
-  });
-    
+$(function()
+{
+    var ticker = function()
+    {
+        setTimeout(function(){
+            $('#ticker li:first').animate( {marginTop: '-20px'}, 400, function()
+            {
+                $(this).detach().appendTo('ul#ticker').removeAttr('style');
+            });
+            ticker();
+        }, 4000);
+    };
+    ticker();
+});
